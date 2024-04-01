1300999c467a5d5696f9eced3949020761c1e066
albinap <albinap952654@gmail.com>
2024-04-01 12:30:22 +0530

    first commit

---
+++ users/urls.py
@@ -0,0 +1,10 @@
+# users/urls.py
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('login/', views.login_view, name='login'),
+    path('signup/', views.signup_view, name='signup'),
+    path('', views.signup_view,name='list'),
+
+]
